53077d7b0cf9b80edbd74d7768b3355974e8960e
--replace-all <qngolg@126.com>
2020-03-12 21:37:42 +0800

    #001 add test case

---
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
@@ -0,0 +1,25 @@
+package cn.xpbootcamp.legacy_code;
+
+import org.junit.jupiter.api.Test;
+
+import javax.transaction.InvalidTransactionException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class WalletTransactionTest {
+
+    @Test
+    public void should_throw_exception_when_buyer_id_is_null() {
+        WalletTransaction walletTransaction = new WalletTransaction("1", null, 1L, 123L, "11");
+        Exception exception = assertThrows(InvalidTransactionException.class, walletTransaction::execute);
+        assertEquals("This is an invalid transaction",exception.getMessage());
+    }
+
+    @Test
+    public void should_throw_a_exception_when_sell_id_is_null() {
+        WalletTransaction walletTransaction = new WalletTransaction("1", 1L, null, 123L, "11");
+        Exception exception = assertThrows(InvalidTransactionException.class, walletTransaction::execute);
+        assertEquals("This is an invalid transaction",exception.getMessage());
+    }
+}
